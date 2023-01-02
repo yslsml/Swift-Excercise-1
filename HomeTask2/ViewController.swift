1be62bfb4bb6db578b3e3a68975915219c8690c5
Milana Antonova <mmf.antonovam@gmail.com>
2023-01-02 23:24:47 +0300

    Initial Commit

---
+++ HomeTask2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeTask2
+//
+//  Created by Milana Antonova on 2.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
